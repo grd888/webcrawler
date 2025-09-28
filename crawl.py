2bcb394e71fe10550a7805148d2de1cb0a0365b7
Greg Delgado <grd888@gmail.com>
2025-09-28 17:05:59 +0800

    feat: add function to extract first paragraph from HTML with main tag priority

--- crawl.py
+++ crawl.py
@@ -13,3 +13,17 @@ def get_h1_from_html(html):
   soup = BeautifulSoup(html, 'html.parser')
   h1 = soup.find('h1')
   return h1.get_text() if h1 else ""
+
+def get_first_paragraph_from_html(html):
+  soup = BeautifulSoup(html, 'html.parser')
+  main = soup.find('main')
+  if main:
+    p = main.find('p')
+    if p:
+      return p.get_text()
+    else:
+      return ""
+  elif soup.find('p'):
+    return soup.find('p').get_text()
+  else:
+    return ""
--- test_extract.py
+++ test_extract.py
@@ -1,5 +1,5 @@
 import unittest
-from crawl import get_h1_from_html
+from crawl import get_h1_from_html, get_first_paragraph_from_html
 
 class TestExtract(unittest.TestCase):
   def test_get_h1_from_html1(self):
@@ -45,6 +45,38 @@ class TestExtract(unittest.TestCase):
     </html>
     """
     actual = get_h1_from_html(html)
+    expected = ""
+    self.assertEqual(actual, expected)
+    
+  def test_get_first_paragraph_from_html_main_priority(self):
+    input_body = '''<html><body>
+        <p>Outside paragraph.</p>
+        <main>
+            <p>Main paragraph.</p>
+        </main>
+    </body></html>'''
+    actual = get_first_paragraph_from_html(input_body)
+    expected = "Main paragraph."
+    self.assertEqual(actual, expected)
+    
+  def test_get_first_paragraph_from_html_no_main_priority(self):
+    input_body = '''<html><body>
+        <p>1st paragraph.</p>
+        
+        <p>2nd paragraph.</p>
+        
+    </body></html>'''
+    actual = get_first_paragraph_from_html(input_body)
+    expected = "1st paragraph."
+    self.assertEqual(actual, expected)
+    
+  def test_get_first_paragraph_from_html_no_paragraph(self):
+    input_body = '''<html><body>
+        <main>
+            <h1>1st paragraph.</h1>
+        </main>
+    </body></html>'''
+    actual = get_first_paragraph_from_html(input_body)
     expected = ""
     self.assertEqual(actual, expected)
 
