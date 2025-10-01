aba64a1f5e3b20a318a150823495c5f849509ec7
Greg Delgado <grd888@gmail.com>
2025-10-02 06:57:08 +0800

    feat: add get_html function with user agent and content type validation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from crawl import get_html
 
 def main():
     if len(sys.argv) < 2:
@@ -10,7 +11,8 @@ def main():
     else:
         BASE_URL = sys.argv[1]
         print(f"starting crawl of: {BASE_URL}")
-        
+        html = get_html(BASE_URL)
+        print(html)
 
 
 if __name__ == "__main__":
